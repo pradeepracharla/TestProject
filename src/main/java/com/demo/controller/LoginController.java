b2865aa8d4143c863b92a152f32921971992f996
Pradeep Racharla <pradeeppradeep6930@gmail.com>
2026-01-06 08:11:57 +0530

    changes are made

--- src/main/java/com/demo/controller/LoginController.java
+++ src/main/java/com/demo/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
@@ -16,7 +18,7 @@ public class LoginController {
 		
 	}
 	
-	@GetMapping("/SignUp")
+	@RequestMapping(value = "/SignUp",method =RequestMethod.GET)
 	public String registeration() {
 		
 		System.out.println("from login controller");
